f359fdfe3530b4cf0ca6e080a9ffac39d56a2788
Rimaria <rimaria_100@hotmail.com>
2018-07-14 19:48:07 -0300

    Aula 3: Listando dados

--- JS_Avancado/JS/main.js
+++ JS_Avancado/JS/main.js
@@ -11,5 +11,23 @@ function getTotal(list){
     }
     return total;
 }
-
 console.log(getTotal(list)); 
+
+function setList(list){
+    var table = '<thead>'+
+                    '<tr><td>Description</td>'+
+                        '<td>Amount</td>'+
+                        '<td>Value</td>'+
+                        '<td>Action</td>'+
+                    '</tr>'+
+                '</thead>'+
+                '<tbody>';
+    for(var key in list){
+        table += '<tr><td>'+list[key].desc+'</td>'+'<td>'+list[key].amount+'</td>'+'<td>'+list[key].value+'</td><td>Edit | Delete</td></tr>';
+    }
+    table += '</tbody>';
+    
+    document.getElementById("listTable").innerHTML = table;
+}
+
+setList(list);
